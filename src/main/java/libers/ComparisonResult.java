8d7ccadd57ac17a9707aa5e348162c0e73be9b93
SovTuch <fedotov8900@gmail.com>
2024-09-15 11:15:33 +0300

    -Added Class ComparisonResult for detectors and comparators

---
+++ src/main/java/libers/ComparisonResult.java
@@ -0,0 +1,39 @@
+package libers;
+
+import java.util.List;
+
+public class ComparisonResult {
+    private List<PackageInfo> inBranch1NotInBranch2;
+    private List<PackageInfo> inBranch1NotInBranch1;
+    private List<PackageInfo> higherInBranch1;
+
+    public ComparisonResult(List<PackageInfo> inBranch1NotInBranch2, List<PackageInfo> inBranch1NotInBranch1, List<PackageInfo> higherInBranch1){
+        this.inBranch1NotInBranch1 = inBranch1NotInBranch1;
+        this.inBranch1NotInBranch2 = inBranch1NotInBranch2;
+        this.higherInBranch1 = higherInBranch1;
+    }
+
+    public List<PackageInfo> getInBranch1NotInBranch2() {
+        return inBranch1NotInBranch2;
+    }
+
+    public void setInBranch1NotInBranch2(List<PackageInfo> inBranch1NotInBranch2) {
+        this.inBranch1NotInBranch2 = inBranch1NotInBranch2;
+    }
+
+    public List<PackageInfo> getInBranch1NotInBranch1() {
+        return inBranch1NotInBranch1;
+    }
+
+    public void setInBranch1NotInBranch1(List<PackageInfo> inBranch1NotInBranch1) {
+        this.inBranch1NotInBranch1 = inBranch1NotInBranch1;
+    }
+
+    public List<PackageInfo> getHigherInBranch1() {
+        return higherInBranch1;
+    }
+
+    public void setHigherInBranch1(List<PackageInfo> higherInBranch1) {
+        this.higherInBranch1 = higherInBranch1;
+    }
+}
